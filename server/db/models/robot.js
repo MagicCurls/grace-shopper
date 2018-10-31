0e630e12176922a61819eb84c93fe55ee57cd4a3
Eric Roginek <eroginek@gmail.com>
2018-10-31 14:39:29 -0400

    Robots api routes created: get all robots; get robot by ID; get robots by brand name

--- server/db/models/robot.js
+++ server/db/models/robot.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 // Defines the Robot product model
-// 'brands' field validates whether its value is equal to a pre-existing company name (taken from the "Featured Brands" tab on Amazon's 'Robots & Robotics' page) using isIn
+// 'brand' field validates whether its value is equal to a pre-existing company name (taken from the "Featured Brands" tab on Amazon's 'Robots & Robotics' page) using isIn
 
 const Robot = db.define('robot', {
   name: {
