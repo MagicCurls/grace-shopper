a0957a7f199cb1aded77dcd791baad30f0aecc62
Khalid Husain <khusain444@gmail.com>
2018-10-30 21:43:18 -0400

    Add route to get all products in a user's cart

--- server/db/models/cartEntry.js
+++ server/db/models/cartEntry.js
@@ -2,14 +2,15 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const CartEntry = db.define('cartEntry', {
-    quantity: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-        validate: {
-            min: 0,
-        }
-    }
+    quantity: Sequelize.INTEGER
+    // quantity: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     defaultValue: 0,
+    //     validate: {
+    //         min: 0,
+    //     }
+    // }
 })
 
 module.exports = CartEntry
