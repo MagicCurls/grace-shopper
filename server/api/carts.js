358b786ab821ae11a3f11df4240c2766000e57b3
Khalid Husain <khusain444@gmail.com>
2018-11-01 12:51:02 -0400

    Fix carts route for getting all of a user's robots in cart

--- server/api/carts.js
+++ server/api/carts.js
@@ -5,19 +5,23 @@ module.exports = router
 
 router.get('/:userId', async (req, res, next) => {
   try {
+    // const userId = Number(req.params.userId);
     const {userId} = req.params;
     const entries = await CartEntry.findAll({where: {userId}});
     const robotIds = entries.map(entry => entry.robotId);
     const robots = await Robot.findAll({
       where: { id: {[Op.in]: robotIds} }
     })
+    const robotMap = {};
+    robots.forEach(robot => {
+      robotMap[robot.dataValues.id] = robot.dataValues;
+    })
     const data = entries.map(entry => {
-      const robotIdx = entry.dataValues.robotId-1;
-      entry.dataValues.robotInfo = robots[robotIdx].dataValues;
+      const robotId = entry.dataValues.robotId;
+      entry.dataValues.robotInfo = robotMap[robotId];
       return entry;
     })
     res.json(data);
-    // User.findById(userId).then(user => user.getRobots()).then(data => res.json(data))
   } catch (err) {
     next(err);
   }
@@ -46,7 +50,7 @@ router.put('/', async (req, res, next) => {
 router.delete('/', async (req, res, next) => {
   try {
     const {userId, robotId} = req.body;
-    CartEntry.findAll({where: {userId, robotId}}).then(entry => entry[0].destroy()).then(res.sendStatus(200));
+    CartEntry.findAll({where: {userId, robotId}}).then(entry => entry[0].destroy()).then(res.sendStatus(201));
   }
   catch (err) {
     next(err);
