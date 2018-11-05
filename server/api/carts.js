2f3b2b1ac5c6745d93a9e26c6e4bc605a863f861
Khalid Husain <khusain444@gmail.com>
2018-11-05 11:58:56 -0500

    Add route security for api/carts

--- server/api/carts.js
+++ server/api/carts.js
@@ -3,56 +3,84 @@ const {CartEntry, Robot, User} = require('../db/models')
 const Op = require('sequelize').Op
 module.exports = router
 
+const isAuthorized = (req, userId) => {
+  return (Number(req.user.dataValues.id) === Number(userId) || req.user.dataValues.isAdmin);
+} 
+
 router.get('/:userId', async (req, res, next) => {
   try {
-    // const userId = Number(req.params.userId);
-    const {userId} = req.params;
-    const entries = await CartEntry.findAll({where: {userId}});
-    const robotIds = entries.map(entry => entry.robotId);
-    const robots = await Robot.findAll({
-      where: { id: {[Op.in]: robotIds} }
-    })
-    const robotMap = {};
-    robots.forEach(robot => {
-      robotMap[robot.dataValues.id] = robot.dataValues;
-    })
-    const data = entries.map(entry => {
-      const robotId = entry.dataValues.robotId;
-      entry.dataValues.robotInfo = robotMap[robotId];
-      return entry;
-    })
-    res.json(data);
+    const {userId} = req.params
+    if (isAuthorized(req, userId)) {
+      const entries = await CartEntry.findAll({where: {userId}})
+      const robotIds = entries.map(entry => entry.robotId)
+      const robots = await Robot.findAll({
+        where: {id: {[Op.in]: robotIds}}
+      })
+      const robotMap = {}
+      robots.forEach(robot => {
+        robotMap[robot.dataValues.id] = robot.dataValues
+      })
+      const data = entries.map(entry => {
+        const robotId = entry.dataValues.robotId
+        entry.dataValues.robotInfo = robotMap[robotId]
+        return entry
+      })
+      res.json(data)
+    }
+    else {
+      res.statusMessage = "Access Forbidden";
+      res.status(403).end();
+    }
   } catch (err) {
-    next(err);
+    next(err)
   }
 })
 
 router.post('/', async (req, res, next) => {
   try {
-    const {userId, robotId, quantity} = req.body;
-    CartEntry.create({userId, robotId, quantity}).then(res.sendStatus(201));
-  }
-  catch (err) {
-    next(err);
+    const {userId, robotId, quantity} = req.body
+    if (isAuthorized(req, userId)) {
+      CartEntry.create({userId, robotId, quantity}).then(res.sendStatus(201));
+    }
+    else {
+      res.statusMessage = "Access Forbidden";
+      res.status(403).end();
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
 router.put('/', async (req, res, next) => {
   try {
-    const {userId, robotId, quantity} = req.body;
-    CartEntry.findAll({where: {userId, robotId}}).then(entry => entry[0].update({quantity})).then(res.sendStatus(201));
-  }
-  catch (err) {
-    next(err);
+    const {userId, robotId, quantity} = req.body
+    if (isAuthorized(req, userId)) {
+      CartEntry.findAll({where: {userId, robotId}})
+      .then(entry => entry[0].update({quantity}))
+      .then(res.sendStatus(201))
+    }
+    else {
+      res.statusMessage = "Access Forbidden";
+      res.status(403).end();
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
 router.delete('/', async (req, res, next) => {
   try {
-    const {userId, robotId} = req.body;
-    CartEntry.findAll({where: {userId, robotId}}).then(entry => entry[0].destroy()).then(res.sendStatus(201));
-  }
-  catch (err) {
-    next(err);
+    const {userId, robotId} = req.body
+    if (isAuthorized(req, userId)) {
+      CartEntry.findAll({where: {userId, robotId}})
+      .then(entry => entry[0].destroy())
+      .then(res.sendStatus(201))
+    }
+    else {
+      res.statusMessage = "Access Forbidden";
+      res.status(403).end();
+    }
+  } catch (err) {
+    next(err)
   }
 })
