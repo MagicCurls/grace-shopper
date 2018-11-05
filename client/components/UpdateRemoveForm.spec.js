003f8ecd0c6d281d9da560f34cf6b647b5fbc14b
Tyler Zoidis <tyler.zoidis@gmail.com>
2018-11-05 18:46:01 -0500

    update remove test attempts

---
+++ client/components/UpdateRemoveForm.spec.js
@@ -0,0 +1,47 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import {UpdateRemoveForm} from './UpdateRemoveForm'
+const sinon = require('sinon')
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('UpdateRemoveForm', () => {
+  let renderedUpdateRemoveForm
+  let updateRemoveFormInstance
+
+  beforeEach(() => {
+    renderedUpdateRemoveForm = shallow(<UpdateRemoveForm />)
+    updateRemoveFormInstance = renderedUpdateRemoveForm.instance()
+  })
+
+  it('should be a dummy component that is passed various props', () => {
+    expect(updateRemoveFormInstance).to.exist
+    expect(updateRemoveFormInstance.props.userId).to.exist
+    expect(updateRemoveFormInstance.props.robotId).to.exist
+    expect(updateRemoveFormInstance.props.cartQuantity).to.exist
+    expect(updateRemoveFormInstance.props.stateQuantity).to.exist
+    expect(updateRemoveFormInstance.props.handleSubmit).to.exist
+    expect(updateRemoveFormInstance.props.handleChange).to.exist
+    expect(updateRemoveFormInstance.props.removeFromCart).to.exist
+  })
+
+  it('should render an <input /> element', () => {
+    expect(renderedUpdateRemoveForm.find('input').node).to.exist
+  })
+
+  it('should have a method called handleChange that is invoked when there is a change event triggered by the <input /> element', () => {
+    expect(typeof updateRemoveFormInstance.handleChange).to.equal('function')
+    const handleChangeSpy = sinon.spy()
+    updateRemoveFormInstance.props.handleChange = handleChangeSpy
+    renderedUpdateRemoveForm.setState({})
+    renderedUpdateRemoveForm.find('input').simulate('change', {
+      target: {value: '10'}
+    })
+    expect(handleChangeSpy.calledOnce).to.equal(true)
+  })
+})
